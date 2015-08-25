24922d432f7b26c51a7ed314840d0ea8bfb01ea4
faberhenrique <faber.henrique@gmail.com>
2015-08-25 14:16:12 -0300

    Aula Atualizada

--- Projetos/LabPoo/LabPoo/Program.cs
+++ Projetos/LabPoo/LabPoo/Program.cs
@@ -5,39 +5,19 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
+
 namespace LabPoo
 {
-    class Criptografia
+    public class inicio
     {
-        public static String CifraCesar(String senha, int key)
+         static void Main()
         {
-            //Removendo Espaços utilizando expressão regular
-            String senhaAjustada = Regex.Replace(senha,@"\s", "");
-            //Considerando apenas o limite do alfabeto
-            int novaKey = key % 26;
+            Criptografia cypher = new Criptografia();
+            string a = "test";
+            string b = cypher.CifraCesar(a, 18); // Ok
+            Console.WriteLine(a);
+            Console.WriteLine(b);
 
-            char[] buffer = senhaAjustada.ToCharArray();
-            for (int i = 0; i < buffer.Length; i++)
-            {
-                // recuperando a Letra.
-                char letter = buffer[i];
-                // Fazendo o Shift
-                letter = (char)(letter + novaKey);
-                // Subtrair 26 caso seja maior que Z.
-                // Aumentar 26 caso letra seja menor que a.
-                if (letter > 'z')
-                {
-                    letter = (char)(letter - 26);
-                }
-                else if (letter < 'a')
-                {
-                    letter = (char)(letter + 26);
-                }
-                // Store.
-                buffer[i] = letter;
-            }
-            return new string(buffer);
         }
-      
     }
 }
---
+++ Projetos/LabPoo/LabPoo/Util/Criptografia.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Criptografia
+{
+
+    public String CifraCesar(String senha, int key)
+    {
+        //Removendo Espaços utilizando expressão regular
+        String senhaAjustada = Regex.Replace(senha, @"\s", "");
+        //Considerando apenas o limite do alfabeto
+        int novaKey = key % 26;
+
+        char[] buffer = senhaAjustada.ToCharArray();
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            // recuperando a Letra.
+            char letter = buffer[i];
+            // Fazendo o Shift
+            letter = (char)(letter + novaKey);
+            // Subtrair 26 caso seja maior que Z.
+            // Aumentar 26 caso letra seja menor que a.
+            if (letter > 'z')
+            {
+                letter = (char)(letter - 26);
+            }
+            else if (letter < 'a')
+            {
+                letter = (char)(letter + 26);
+            }
+            // Store.
+            buffer[i] = letter;
+        }
+        return new string(buffer);
+    }
+}
