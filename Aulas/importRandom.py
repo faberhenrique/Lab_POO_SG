f13a5f14af522d936aec9b1aa29cf628ba9ed0c1
Faber Xavier <faber.henrique@gmail.com>
2024-05-21 15:58:40 -0300

    Teste2

--- Aulas/importRandom.py
+++ Aulas/importRandom.py
@@ -1,3 +1,12 @@
+import random
+
+# Generate a random number between 1 and 10
+random_number = random.randint(1, 10)
+
+# Print the random number
+print("Random number:", random_number)
+
+
 print("Hello, World!")
 
 for num in range(1, 101):
