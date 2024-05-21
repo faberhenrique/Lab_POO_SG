ab51280167cfb832b3b82c7fc9da07e8ae85d3d3
Faber Xavier <faber.henrique@gmail.com>
2024-05-21 15:54:35 -0300

    Teste de PR

--- Aulas/test.py
+++ Aulas/test.py
@@ -1,1 +1,11 @@
 print("Hello, World!")
+
+for num in range(1, 101):
+    if num % 3 == 0 and num % 5 == 0:
+        print("FuzzyBuzz")
+    elif num % 3 == 0:
+        print("Fuzzy")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
